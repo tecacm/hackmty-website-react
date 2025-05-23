0e0c78bedcec73259648e92607d5b7d1bf760c75
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-05-22 22:15:22 -0600

    update base

--- hackmty-site/vite.config.ts
+++ hackmty-site/vite.config.ts
@@ -5,6 +5,6 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/hackmty-website-react/',
+  base: '/',
 })
 
