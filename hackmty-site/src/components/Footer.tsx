ba9bff7e61744d5c123f572096bafcf861526d4e
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-09-12 23:10:28 -0600

    addded discord to page

--- hackmty-site/src/components/Footer.tsx
+++ hackmty-site/src/components/Footer.tsx
@@ -4,6 +4,7 @@ import SvgIcon from '@mui/material/SvgIcon';
 
 import FacebookIcon from '../assets/media/facebook.svg?react';
 import InstagramIcon from '../assets/media/instagram.svg?react';
+import DiscordIcon from '../assets/media/discord.svg?react';
 import { BottomNavigation } from '@mui/material';
 
 import TecACMLogo from "../assets/tec-acm-colorless.svg?react";
@@ -19,8 +20,11 @@ function Footer() {
                   <SvgIcon component={FacebookIcon} inheritViewBox sx={{fontSize:30, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}, mr:4}}/>
               </Box>
               <Box component="a" href="https://instagram.com/hackmty/" target="_blank">
-                  <SvgIcon component={InstagramIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}}}/>
+                  <SvgIcon component={InstagramIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main'}, mr: 4}}/>
               </Box>
+                <Box component="a" href="https://hackmty.com/discord" target="_blank">
+                    <SvgIcon component={DiscordIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}}}/>
+                </Box>
             </Box>
             <Typography color='white' component="a" href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf' sx={{transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}}}>Code of Conduct</Typography>
             <Box component="a" href="https://tec.acm.org" target="_blank" sx={{ display: 'flex', alignItems: 'center', justifyContent:'center' }}>
--- hackmty-site/src/components/Navbar.tsx
+++ hackmty-site/src/components/Navbar.tsx
@@ -14,6 +14,7 @@ import SvgIcon from '@mui/material/SvgIcon';
 import { MLHTrustBadge } from './MLHTrustBadge';
 import FacebookIcon from '../assets/media/facebook.svg?react';
 import InstagramIcon from '../assets/media/instagram.svg?react';
+import DiscordIcon from '../assets/media/discord.svg?react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { darken } from '@mui/material';
@@ -132,10 +133,13 @@ function Navbar() {
               </MenuItem>
 
               <Box component="a" href="https://facebook.com/HackMTY/" target="_blank" paddingLeft={1.5}>
-                <SvgIcon component={FacebookIcon} inheritViewBox sx={{fontSize:30, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}, mr:4}}/>
+                <SvgIcon component={FacebookIcon} inheritViewBox sx={{fontSize:30, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main'}, mr:4}}/>
               </Box>
               <Box component="a" href="https://instagram.com/hackmty/" target="_blank">
-                <SvgIcon component={InstagramIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}}}/>
+                <SvgIcon component={InstagramIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main'}, mr: 4}}/>
+              </Box>
+               <Box component="a" href="https://hackmty.com/discord" target="_blank">
+                <SvgIcon component={DiscordIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}}}/>
               </Box>
             </Menu>
           </Box>
@@ -175,7 +179,10 @@ function Navbar() {
                 <SvgIcon component={FacebookIcon} inheritViewBox sx={{fontSize:30, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}, mr:4}}/>
             </Box>
             <Box component="a" href="https://instagram.com/hackmty/" target="_blank">
-                <SvgIcon component={InstagramIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}}}/>
+                <SvgIcon component={InstagramIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main'}, mr: 4}}/>
+            </Box>
+            <Box component="a" href="https://hackmty.com/discord" target="_blank">
+                <SvgIcon component={DiscordIcon} inheritViewBox sx={{fontSize:27, color:'white', transition: 'color 0.3s ease', '&:hover': { color: 'secondary.main',}}}/>
             </Box>
           </Box>
           
