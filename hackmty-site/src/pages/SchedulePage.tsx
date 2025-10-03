e9eb160593d782a7a4724ea59e7d846f4f383cdb
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-10-02 23:44:54 -0600

    added timeline

--- hackmty-site/src/pages/SchedulePage.tsx
+++ hackmty-site/src/pages/SchedulePage.tsx
@@ -12,8 +12,8 @@ function SchedulePage() {
             <FeatherRain/>
             <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
                 <TitleCard title={t('schedule.title', 'Schedule')}/>
-                <Box display='flex' sx={{flexDirection:{xs:'column', md:'row'}}} gap={{xs: '2rem', md: '3vw'}} marginTop={'5vh'} alignItems={{xs: 'center', md: 'stretch'}} mx={'5vw'}>
-                    <InformationCard title={t('schedule.comingSoon', 'Coming Soon')} description={t('schedule.stayTuned', 'Stay tuned to our social media!')}></InformationCard>
+                <Box display='flex' sx={{flexDirection:'row', width:'80%'}} gap={{xs: '2rem', md: '3vw'}} marginY={'5vh'} alignItems={{xs: 'center', md: 'stretch'}} mx={'5vw'}>
+                    <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=v2%3A2PACX-1vSy30Wadov_Tzsk8Km7sFpPa1tgYGvybv-vfiPHGX3LGwmQPPjKMv2GYXUJByYXi_HGw1rpZ3etjlvO&font=https://fonts.googleapis.com/css2?family=Montserrat&lang=en&initial_zoom=2&width=100%25&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
                 </Box>
             </Box>
         </Box>
